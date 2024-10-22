09d75b3b059a722e70d5f090283f27f7edc42c16
DariusKabore <darius.kabore@stu.ocsb.ca>
2024-10-22 14:22:01 +0000

    1 more

--- assignment.py
+++ assignment.py
@@ -1,1 +1,8 @@
-
+import math
+print("Calculates Area and Circumference of a Circle")
+diameter = float(float(input("Enter Diameter: "))) 
+radius = diameter/2
+area = (radius**2)*(math.pi)
+circumference = radius*math.pi*2
+print("Area: ",area, "un²")
+print("Circumference: ",circumference)
